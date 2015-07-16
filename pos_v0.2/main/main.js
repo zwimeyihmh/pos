7e0703d88decfe2d0ee2fe86254f30d66fd260e6
zwimeyihmh <zwimeyihmh@outlook.com>
2015-07-16 22:18:25 +0800

    pos_v1

--- pos_v0.2/main/main.js
+++ pos_v0.2/main/main.js
@@ -1,23 +1,20 @@
 //TODO: Please write code in this file.
-function printReceipt(inputs){
+function printReceipt(inputs) {
   var newInput = [];
-  for(var i = 0; i < inputs.length; i++){
+  for(var i = 0; i < inputs.length; i++) {
     match(inputs[i],newInput);
   }
-  console.log(newInput);
   printInput(newInput);
 
 }
-function match(item,inputs){
+function match(item,inputs)  {
   var allItems = loadAllItems();
-  for(var x = 0; x < allItems.length; x++){
+  for(var x = 0; x < allItems.length; x++) {
     if(allItems[x].barcode === item) {
-      if(inputs.length > 0 && inputs[inputs.length-1].name === allItems[x].name){
+      if(inputs.length > 0 && inputs[inputs.length-1].name === allItems[x].name) {
         (inputs[inputs.length-1].count)++;
         break;
-      }
-      // if(NotInputs(allItems[x].name,inputs)){
-      else{
+      } else {
         inputs.push({name:allItems[x].name,count:1,unit:allItems[x].unit,price:allItems[x].price});
         break;
       }
@@ -46,56 +43,3 @@ function printInput(inputs) {
   result += '----------------------\n总计：' + sum + '.00(元)\n**********************'
   console.log(result);
 }
-
-
-
-
-/*
-function printReceipt(inputs) {
-  var newInputs = [];
-  for(var i = 0; i < inputs.length; i++){
-    matchId(inputs[i],newInputs);
-  }
-
-
-  printInputs(newInputs);
-  //PrintInputs(newInputs);
-}
-function matchId(input,inputs)
-{
-  var allItems = loadAllItems();
-  for(var i = 0; i < allItems.length; i++){
-    if(input === allItems[i].barcode){
-      inputs.push(allItems[i]);
-      break;
-    }
-  }
-}
-
-
-function printInputs(inputs) {
-  var result = '***<没钱赚商店>收据***\n';
-  var sum = 0;
-  for(var i = 0; i < inputs.length; i = i+count)
-  {
-    var count = SameName(i,inputs);
-    var allMoney = count * inputs[i].price;
-    sum += allMoney;
-    result += '名称：' + inputs[i].name + '，数量：' + count + inputs[i].unit + '，单价：'+ inputs[i].price + '.00(元)，小计：' +   allMoney+'.00(元)\n';
-  }
-  result += '----------------------\n总计：' + sum + '.00(元)\n**********************'
-  console.log(result);
-}
-function SameName(i,inputs){
-  var count = 1;
-  for(var x = i+1; x < inputs.length; x++)
-  {
-    if(inputs[i].name === inputs[x].name)
-    {
-      count++;
-    }
-  }
-  return count;
-}
-*/
-
